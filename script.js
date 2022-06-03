d626f53273b194ba2daf69fd3d49a0266e871181
Katoma610 <55027295+Katoma610@users.noreply.github.com>
2022-06-03 16:56:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("horray!!!");
+
+let timer;
+let titleOld = document.querySelector('head title').innerHTML;
+
+function changeTitle(icon, text){
+    document.querySelector('head title').innerHTML = text;
+    document.querySelector('link[rel="icon"]').setAttribute('href', icon);
+}
+
+window.onblur = () => {
+    timer = setTimeout(() => {
+        changeTitle('img/emoji.jpg', '😭😭Вернись пожалуйста😭😭')
+    }, 3000);
+}
+
+window.onfocus = () => {
+    clearTimeout(timer);
+    changeTitle('img/icon.jpg', titleOld);
+}
+
+let button = document.querySelector('.buttonMedia');
+let header = document.querySelector('.siteHeader');
+
+button.addEventListener('click', function(){
+    header.classList.add("siteHeader-bg");
+    console.log("fdsjlkjfdsjk");
+});
